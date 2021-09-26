997a3ff44fb78d1e842959c2dfc7c27cc31bd1c6
AlexandrVinogradov <ale.grapes.webdev@gmail.com>
2021-09-27 00:38:40 +0300

    remove garbage

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,13 @@
 console.log("its work");
 
-// styles for swiper
-// import 'swiper/swiper-bundle.css';
-
- // scripts for header + garbage 
-// import './main.js'
-
-// js/scripts
-// import './special-project.js'
-// import './main-page-sliders.js'
-// import './search.js'
-// import './default-dropdown.js'
-// import './question-modals.js'
-// import './default-popup.js'
-// import './offer-news-success-popup.js'
-
 // import './wow-min.js'
-import './new/jquery.arcticmodal-0.3.min.js'
-import './new/jquery.malihu.PageScroll2id.min.js'
-import './new/jquery.smooth-scroll.min.js'
-import './new/lightbox.min.js'
-import './new/parallax.min.js'
-import './new/popUp.js'
-import './new/prettySticky.js'
-import './new/scripts.js'
+import './jquery.arcticmodal-0.3.min.js'
+import './jquery.malihu.PageScroll2id.min.js'
+import './jquery.smooth-scroll.min.js'
+import './lightbox.min.js'
+import './parallax.min.js'
+import './popUp.js'
+import './prettySticky.js'
+import './scripts.js'
 // import './new/slick.min.js'
 
