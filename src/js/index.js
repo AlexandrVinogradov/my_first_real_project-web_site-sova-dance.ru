651ba62ef3089ce88a17c00cf976540c7b38d40f
AlexandrVinogradov <ale.grapes.webdev@gmail.com>
2021-09-27 21:40:45 +0300

    remove imgs

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 console.log("its work");
 
-// import './wow-min.js'
+// import './wow-min.js' подключено через cnd в начале странице
 import './jquery.arcticmodal-0.3.min.js'
 // import './jquery.malihu.PageScroll2id.min.js' //- лишнее?
 // import './slick.min.js' //- подключено через cnd в конце man-page
