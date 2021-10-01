0a745b200ade787b3fa049b7ab9491ca21ac02ef
AlexandrVinogradov <ale.grapes.webdev@gmail.com>
2021-10-01 13:45:05 +0300

    perya via html

--- src/js/index.js
+++ src/js/index.js
@@ -8,7 +8,7 @@
 import './jquery.arcticmodal-0.3.min.js'
 // import './jquery.malihu.PageScroll2id.min.js' //- лишнее?
 import './jquery.smooth-scroll.min.js'
-// import './lightbox.min.js' // лишнее?
+import './lightbox.min.js' // лишнее?
 import './parallax.min.js'
 import './popUp.js'
 import './prettySticky.js'
